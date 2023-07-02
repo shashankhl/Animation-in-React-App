3070f1726b887476d18c5fb9a361e14e4abab496
shashankhl <shashankhl280@gmail.com>
2023-07-02 13:20:42 +0530

    Intial commit

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+import "./Modal.css";
+import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
+
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  // const cssClasses = [
+  //   "Modal",
+  //   props.show === "entering"
+  //     ? "ModalOpen"
+  //     : props.show === "exiting"
+  //     ? "ModalClosed"
+  //     : null,
+  // ];
+
+  return (
+    // <div className={cssClasses.join(" ")}>
+    //   <h1>A Modal</h1>
+    //   <button className="Button" onClick={props.closed}>
+    //     Dismiss
+    //   </button>
+    // </div>
+
+    // <Transition
+    //   in={props.show}
+    //   timeout={animationTiming}
+    //   mountOnEnter
+    //   unmountOnExit
+    // >
+    //   {(state) => {
+    //     const cssClasses = [
+    //       "Modal",
+    //       state === "entering"
+    //         ? "ModalOpen"
+    //         : state === "exiting"
+    //         ? "ModalClosed"
+    //         : null,
+    //     ];
+    //     return (
+    //       <div className={cssClasses.join(" ")}>
+    //         <h1>A Modal</h1>
+    //         <button className="Button" onClick={props.closed}>
+    //           Dismiss
+    //         </button>
+    //       </div>
+    //     );
+    //   }}
+    // </Transition>
+
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      // classNames="fade-slide"
+      classNames={{
+        enter: "",
+        enterActive: "ModalOpen",
+        exit: "",
+        exitActive: "ModalClosed",
+      }}
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default modal;
